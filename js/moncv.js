2edc681209ea2b333497734826b4f95f9da35f2a
Marco 🐝 <marc.pouliquen@orange.fr>
2023-08-11 16:27:08 +0200

    header fixes

--- js/moncv.js
+++ js/moncv.js
@@ -1,54 +1,17 @@
 $(function()
 {
-    //////////////////////////////////////////////////////////////
-    //  Gestion du scroll horizontal pour la section expériences//
-    //////////////////////////////////////////////////////////////
-    
-    $('a.panel').click(function () {
-        //reset and highlight the clicked link
-        $('a.panel').removeClass('selected');
-        $(this).addClass('selected');
-        
-        //grab the current item, to be used in resize function
-        current = $(this);
-        
-        //scroll it to the destination
-        $('#wrapper').scrollTo($(this).attr('href'), 800, { axis:'x' });        
-        
-        //set visible or hidden the bottom menu
-        if($(this).attr('href')=='#e0'){
-            $('#menuExperiences').removeClass('visible').addClass('hidden');
-        }
-        else{
-            $('#menuExperiences').removeClass('hidden').addClass('visible');
-        }
-        
-        //Color in blue the current item (when the user comes from the initial menu)
-        if($(this).attr('href')=='#e0'){
-            $('a.p0').addClass('selected');
-        }
-        else if($(this).attr('href')=='#e1'){
-            $('a.p1').addClass('selected');
-        }
-        else if($(this).attr('href')=='#e2'){
-            $('a.p2').addClass('selected');
-        }
-        else if($(this).attr('href')=='#e3'){
-            $('a.p3').addClass('selected');
-        }
-            
-        //cancel the link default behavior
-        return false;
-    });
-    
     ///////////////////////////////////////////
     //  Gestion du scroll vertical du header //
     ///////////////////////////////////////////
     
     $('a.lienheader').click(function(){  
         var the_id = $(this).attr("href");  
+        var headerHeight = $('#header').height() - 1;
+        if (! $('#header').is(':visible')) {
+            headerHeight = 0;
+        }
         $('html, body').animate({  
-            scrollTop:$(the_id).offset().top-39  
+            scrollTop:$(the_id).offset().top-headerHeight  
         }, 'slow');  
         return false;  
     });
@@ -65,7 +28,11 @@ $(function()
 	// On récupère notre élément
 	var menu = $('#headerfix');
         var lienHeader = $('a.lienheader');
-        var hauteur = $('#top').height()+39;
+        var headerHeight = $('#header').height() - 1;
+        if (! $('#header').is(':visible')) {
+            headerHeight = 0;
+        }
+        var hauteur = $('#top').height()+headerHeight;
         var photo = $('#mainphoto');
         var opac = 1-scrollTop/700;
                 
